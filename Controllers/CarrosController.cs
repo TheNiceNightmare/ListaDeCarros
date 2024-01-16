5f7a42aaf16b8741e6dd09fa0e99421afde539e4
GizloDani <daniel.olivo@gizlocorp.com>
2024-01-16 11:21:36 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CarrosController.cs
@@ -0,0 +1,54 @@
+﻿using ListaDeCarros.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeCarros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrosController : ControllerBase
+    {
+
+        private static List<Carro> _carro = new List<Carro>
+        {
+            new Carro
+            {
+                Id = 1,
+                Brand = "Audi",
+                Model = "Lexus LS"
+
+            },
+            new Carro
+            {
+                Id = 2,
+                Brand = "Lamborghini",
+                Model = "Veneno Roadster"
+
+            },
+            new Carro
+            {
+                Id = 3,
+                Brand = "Ford",
+                Model = "Shelby 260 Cobra de 1962"
+
+            },
+
+             new Carro
+            {
+                Id = 4,
+                Brand = "Mercedes-Benz",
+                Model = "300 SLR Uhlenhaut Coupé"
+
+            },
+        };
+
+        [HttpGet]
+
+        public IEnumerable<Carro> GetAllCarro()
+        {
+            return _carro;
+        }
+
+    }
+}
+    
---
+++ Models/Carro.cs
@@ -0,0 +1,12 @@
+﻿namespace ListaDeCarros.Models
+{
+    public class Carro
+    {
+
+        public int Id { get; set; }
+        public String Brand { get; set; }
+
+        public String Model { get; set; }
+
+    }
+}
